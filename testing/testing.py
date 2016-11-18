2a6df0a05c9cad184904773bea17e5f2d76d33fc
tman <teemuka90@gmail.com>
2016-11-18 15:25:30 +0200

    fixed issue with different image type

--- testing/testing.py
+++ testing/testing.py
@@ -1,11 +1,13 @@
 import skimage.filters as sf
 from skimage.io import imsave, imread
 from skimage.feature import canny
-from scipy.ndimage.filters import sobel
+from scipy.ndimage.filters import sobel, gaussian_filter
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
+
+eps = 10e-6 #for division of zero stuff
 
 #REMINDER: dy = rows, first in numpy indexing, dx = columns, second in numpy indexing
 
@@ -15,17 +17,14 @@ def plot_img(image):
 def check_edge_grad(source_grads, target_grads):
 	#check if opposite diection, then if the orientation is right?
 	sdy, sdx = source_grads; tdy, tdx = target_grads
-	#if they have different signs they are opposite?
-	eps = 10e-6
 	source_ori = np.arctan(sdy/(sdx+eps)); target_ori = np.arctan(tdy/(tdx+eps))
-	#if source_ori + target_ori < np.pi/6:
-		#accept as opposite candidate
-	return True
+	if source_ori - target_ori < np.pi/4: #accept as opposite candidate if true (what about direction?)
+		return True
 
 	return False
 
 
-def get_steps(edges, dxs, dys):
+def get_swts(edges, dxs, dys):
 	posy, posx = np.where(edges)
 	edgeset = set(zip(posy, posx)) #for checking if weve hit another edgepixel in the line
 	stroke_widths = np.zeros_like(edges).astype(np.float64)
@@ -37,15 +36,16 @@ def get_steps(edges, dxs, dys):
 		this_x = posx[i]; this_y = posy[i]
 		dy = dys[this_y, this_x]; dx = dxs[this_y, this_x]
 		source_grad = (dy, dx)
-		xs = [this_x]; ys = [this_y]
+		#xs = [this_x]; ys = [this_y]
+		xs = []; ys = []
 
-		for step in range(1, 25):
+		for step in range(1, 15):
 			incrx = int(dx*step); incry = int(dy*step)
 			newx = this_x + incrx; newy = this_y + incry
 			if newx >= maxx or newy >= maxy or newy < 0 or newx < 0: break
 
-			xs.append(newx); ys.append(newy) #need these to possibly assign new values to these pixels
-			if (newy, newx) in edgeset: #we hit another edge
+			#xs.append(newx); ys.append(newy) #do we include edge pixels or not?
+			if (newy, newx) in edgeset and len(xs) > 0: #we hit another edge
 				target_grad = (dys[newy, newx], dxs[newy, newx])
 				if check_edge_grad(source_grad, target_grad):
 					assign_pixels = (np.array(ys), np.array(xs))
@@ -55,6 +55,8 @@ def get_steps(edges, dxs, dys):
 
 				break
 
+			xs.append(newx); ys.append(newy) #need these to possibly assign new values to these pixels
+
 	for ray in nondiscarded_rays:
 		median = np.median(stroke_widths[ray])
 		stroke_widths[ray] = np.minimum(stroke_widths[ray], median)
@@ -63,14 +65,18 @@ def get_steps(edges, dxs, dys):
 	return stroke_widths
 
 	
-img = imread("../images/I.png", as_grey=True)
-
-dx = sobel(img, 1).astype(np.float64) *-1 #maybe multiply with -1, easier to think that way (normally white = 1, black = 0)
-dy = sobel(img, 0).astype(np.float64) *-1
+img = imread("../images/testimage.png", as_grey=True).astype(np.float64)
+img /= np.max(img)
+#img = np.array(Image.open("../images/test2.png").convert("L"))
+filtered = gaussian_filter(img, sigma=0.5, truncate=3) #5x5 filter
+dx = sobel(img, 1).astype(np.float64) * -1 #maybe multiply with -1, easier to think that way (normally white = 1, black = 0)
+dy = sobel(img, 0).astype(np.float64) * -1
 maxes = np.max(np.abs(np.stack([dx, dy])), axis=0) #for normalizing gradients so the bigger is 1, easier to step
 maxes[maxes==0] = 1.0 #if both are zero max is zero, fix divide by zero (number doesnt matter here as zero will be divided)
+magnitudes = np.hypot(dx, dy)
 dx = dx/maxes
 dy = dy/maxes
-edges = canny(img).astype(np.int32)
+edges = canny(img, low_threshold=0.3, high_threshold=0.9, sigma=0.5).astype(np.int32)
 
-swts = get_steps(edges, dx, dy)
+swts = get_swts(edges, dx, dy)
+plot_img(swts)
